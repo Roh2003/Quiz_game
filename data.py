29ab4be972e6f6748c6785864e7b3a2ffe8e29f9
Roh2003 <149323646+Roh2003@users.noreply.github.com>
2024-08-07 23:21:09 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,63 @@
+data_list = [
+    {
+        "question":"Which planet is known as the Red Planet?",
+        "choices":["Earth","Jupiter","Mars","Saturn"],
+        "answer":"Mars"
+
+    },
+    {
+        "question":"Who wrote To Kill a Mockingbird ?",
+        "choices":["J.K. Rowling","Harper Lee","Mark Twain","Ernest Hemingway"],
+        "answer":"Harper Lee"
+
+    },
+    {
+        "question":"What is the largest mammal in the world?",
+        "choices":["Elephant","Blue Whale","Great white Shark","Giraffe"],
+        "answer":"Blue Whale"
+
+    },
+    {
+        "question":"What is the chemical symbol for gold?",
+        "choices":["Au","Ag","fe","Pb"],
+        "answer":"Au"
+
+    },
+    {
+        "question":"Which element has the atomic number 1?",
+        "choices":["Hilium","Hydrogen","Lithium","Berylium"],
+        "answer":"Hydrojen"
+
+    },
+    {
+        "question":"In which year did the Titanic sink?",
+        "choices":["1905","1912","1920","1889"],
+        "answer":"1912"
+
+    },
+    {
+        "question":"What is the capital of Japan?",
+        "choices":["Seoul","Bijling","Tokyo","Bangkok"],
+        "answer":"Tokyo"
+
+    },
+    {
+        "question":"Who Painted The Mona Lisa? ",
+        "choices":["Vincent","Pablo Picasso","Leonardo de vinci","Claude Monet"],
+        "answer":"Leonardo de Vinci"
+
+    },
+    {
+        "question":"Which Planet is Closest to the sun ",
+        "choices":["Earth","Venus","mercury","Mars"],
+        "answer":"mercury"
+
+    },
+    {
+        "question":"What is the tallest mountain in the world ",
+        "choices":["k2","Mount Everest","Lhotse","kangchan junga"],
+        "answer":"Mount Everest"
+
+    },
+
+]
